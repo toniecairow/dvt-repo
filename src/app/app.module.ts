52ed20a8560a27845b4b79b6e5e94cadba70a74c
toniecairow <toniecairow@gmail.com>
2022-06-06 21:43:08 +0300

    initialises top-nav,album and track components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArtistsListViewComponent } from './pages/artists-list-view/artists-list-view.component';
 import { ArtistsDetailViewComponent } from './pages/artists-detail-view/artists-detail-view.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { TrackComponent } from './components/track/track.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistsListViewComponent,
-    ArtistsDetailViewComponent
+    ArtistsDetailViewComponent,
+    TopNavComponent,
+    TrackComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
