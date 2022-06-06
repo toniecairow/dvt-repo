b08ed6e8d6e14b04f2d5cf9462a92a2278c22ae6
toniecairow <toniecairow@gmail.com>
2022-06-06 21:50:12 +0300

    initialises the routing rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistsDetailViewComponent } from './pages/artists-detail-view/artists-detail-view.component';
+import { ArtistsListViewComponent } from './pages/artists-list-view/artists-list-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'artists',
+    pathMatch: 'full'
+  },
+  {
+    path: 'artists',
+    component: ArtistsListViewComponent
+  },
+  {
+    path: 'artists/:id',
+    component: ArtistsDetailViewComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'artists'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
