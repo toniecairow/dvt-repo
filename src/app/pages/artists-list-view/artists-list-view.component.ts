fe5793f886220d6f681713e20ec31afc85df7c4e
toniecairow <toniecairow@gmail.com>
2022-06-07 03:26:48 +0300

    finalizes on the artists list view page

--- src/app/pages/artists-list-view/artists-list-view.component.ts
+++ src/app/pages/artists-list-view/artists-list-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-artists-list-view',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artists-list-view.component.scss']
 })
 export class ArtistsListViewComponent implements OnInit {
+  filteredArtists: Observable<any[]> = new Observable();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onArtistsRetrieved($event: Observable<any[]>){
+    this.filteredArtists = $event;
   }
 
 }
